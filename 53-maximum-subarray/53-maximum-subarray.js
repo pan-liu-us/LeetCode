499c3b0db382a31105ade5a91c23f00c6e3c517d
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-08-30 01:00:14 -0700

    Time: 112 ms (65.66%), Space: 52 MB (10.23%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,23 +3,18 @@
  * @return {number}
  */
 
-// dp:
-// 首先对数组进行遍历，当前最大连续子序列和为 sum，结果为 ans
-// 如果 sum > 0，则说明 sum 对结果有增益效果，则 sum 保留并加上当前遍历数字
-// 如果 sum <= 0，则说明 sum 对结果无增益效果，需要舍弃，则 sum 直接更新为当前遍历数字
-// 每次比较 sum 和 ans的大小，将最大值置为ans，遍历结束返回结果
-
+// dp动态规划转移方程: f(i)=max{f(i−1)+nums[i],nums[i]}
+// 时间复杂度 O(n); 空间复杂度 O(n)
 
 var maxSubArray = function(nums) {
-  let ans = nums[0];
-  let sum = 0;
-  for(const num of nums) {
-    if(sum > 0) {
-      sum += num;
-    } else {
-      sum = num;
+  let dp = new Array(nums.length);
+  dp[0] = nums[0];
+  let max = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+    if (max < dp[i]) {
+      max = dp[i];
     }
-    ans = Math.max(ans, sum);
   }
-  return ans;
+  return max;
 };
