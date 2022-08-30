6499de6f84a85d412001bbd36c0832b3bdab80d8
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-08-30 10:54:59 -0700

    Time: 86 ms (89.48%), Space: 18.7 MB (97.09%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -5,19 +5,27 @@
 #         self.left = None
 #         self.right = None
 
-# Recursive Approach using Python Time:O(N) Space:O(N)
+# Iterative Approach using Python Time:O(N) Space:O(1)
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        parent_val = root.val
         p_val = p.val
         q_val = q.val
 
-        # If both p and q are greater than parent
-        if p_val > parent_val and q_val > parent_val:    
-            return self.lowestCommonAncestor(root.right, p, q)
-        # If both p and q are lesser than parent
-        elif p_val < parent_val and q_val < parent_val:    
-            return self.lowestCommonAncestor(root.left, p, q)
-        # We have found the split point, i.e. the LCA node.
-        else:
-            return root
+        # Start from the root node of the tree
+        node = root
+
+        # Traverse the tree
+        while node:
+
+            # Value of current node or parent node.
+            parent_val = node.val
+
+            if p_val > parent_val and q_val > parent_val:    
+                # If both p and q are greater than parent
+                node = node.right
+            elif p_val < parent_val and q_val < parent_val:
+                # If both p and q are lesser than parent
+                node = node.left
+            else:
+                # We have found the split point, i.e. the LCA node.
+                return node
