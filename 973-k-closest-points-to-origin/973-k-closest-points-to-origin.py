a78042da12c83f8110346710db0dc77c20576ed5
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-08-31 12:53:36 -0700

    Time: 2141 ms (5.01%), Space: 20.4 MB (61.60%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,10 +1,29 @@
-# Approach 1: Sort 
-# Time Complexity: O(NlogN)
+# Approach 2: Divide and Conquer
+# Time Complexity: O(N)
 # Space Complexity: O(N)
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        points.sort(key=lambda x: (x[0] ** 2 + x[1] ** 2))
+        def random_select(left: int, right: int, k: int):
+            pivot_id = random.randint(left, right)
+            pivot = points[pivot_id][0] ** 2 + points[pivot_id][1] ** 2
+            points[right], points[pivot_id] = points[pivot_id], points[right]
+            i = left - 1
+            for j in range(left, right):
+                if points[j][0] ** 2 + points[j][1] ** 2 <= pivot:
+                    i += 1
+                    points[i], points[j] = points[j], points[i]
+            i += 1
+            points[i], points[right] = points[right], points[i]
+            # [left, i-1] 都小于等于 pivot, [i+1, right] 都大于 pivot
+            if k < i - left + 1:
+                random_select(left, i - 1, k)
+            elif k > i - left + 1:
+                random_select(i + 1, right, k - (i - left + 1))
+
+        n = len(points)
+        random_select(0, n - 1, k)
         return points[:k]
 
+            
         
