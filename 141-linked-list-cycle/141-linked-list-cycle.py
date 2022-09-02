8ac57a5bafdfdd1ed2a8fb5ad8aa8f75e7a21540
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-01 17:56:52 -0700

    Time: 134 ms (6.32%), Space: 17.5 MB (66.77%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -4,18 +4,27 @@
 #         self.val = x
 #         self.next = None
 
-# Approach 1: Hash Table - check whether a node had been visited before
+# Approach 2: Fast and Slow Pointer: Floyd's Cycle Detection Algorithm
+# no cycle: the fast pointer will eventually reach the end, return false 
+# has cycle: the fast runner will eventually meet the slow runner, return true
+
 # Time complexity: O(n)
-# Space complexity: O(n)
+# Space complexity: O(1)
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        seen = set()
-        while head:
-            if head in seen:
-                return True
-            seen.add(head)
-            head = head.next
-        return False
+        if head == None or head.next == None:
+            return False
+        
+        slow = head
+        fast = head.next
+
+        while slow != fast:
+            if fast == None or fast.next == None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        
+        return True
 
         
