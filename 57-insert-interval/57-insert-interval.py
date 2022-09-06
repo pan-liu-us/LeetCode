603078d5e36b75d10b3fcf5eb594f37a053643d7
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-06 08:06:49 -0700

    Time: 135 ms (42.40%), Space: 17.2 MB (91.92%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.py
@@ -0,0 +1,27 @@
+# Time complexity: O(N)
+# Space complexity: O(N)
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n = len(intervals)
+        i, output = 0, []
+        
+        # Append first non-overlapping intervals
+        while i < n and intervals[i][1] < newInterval[0]:
+            output.append(intervals[i])
+            i += 1
+        
+        # Merge overlapping intervals (if no overlapping then just append newInterval)
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(intervals[i][0], newInterval[0])
+            newInterval[1] = max(intervals[i][1], newInterval[1])
+            i += 1
+        output.append(newInterval)
+        
+        # Append remaining non-overlapping intervals
+        while i < n:
+            output.append(intervals[i])
+            i += 1
+            
+        return output
+        
