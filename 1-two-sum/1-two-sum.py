7f9c769634043be924b54b3ccb3aac39a66a4113
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-07 14:52:27 -0700

    Time: 94 ms (69.27%), Space: 15.3 MB (23.96%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,14 @@
+# One-pass Hash Table => val: index
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
+
+  
