bafaf2d9e7fe3152fa9c008157371374b0b426f6
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-06 23:28:00 -0700

    Time: 49 ms (43.56%), Space: 13.9 MB (56.84%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,17 +1,15 @@
-# Approach: Dynamic Programming
-# The total number of ways to reach ith step = sum of ways of reaching (i-1)th and (i-2)th step
-# Example: n = 4, dp = [0,1,2,3,5], dp[n] = 5
-    
+# Approach: Dynamic Programming & Rolling Array => Fibonacci Number
+# Time complexity: O(n)
+# Space complexity: O(1)
+
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1 or n == 2:
             return n
-
-        dp = [0 for _ in range(n + 1)]
-        dp[1], dp[2] = 1, 2
+        
+        first, second = 1, 2
 
         for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-
-        return dp[n]
+            first, second = second, first + second
+        return second
         
