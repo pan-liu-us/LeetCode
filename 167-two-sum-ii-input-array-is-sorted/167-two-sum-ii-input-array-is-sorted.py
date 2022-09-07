550bf1851300fba0c8275e3c1a99820d40e801d8
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-07 16:49:28 -0700

    Time: 287 ms (12.08%), Space: 14.9 MB (41.21%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,12 +1,15 @@
-# Approach 1: One-pass Hash Table => val: index
+# Approach 2: Two Pointers
 # Time complexity: O(n)
-# Space complexity: O(n)
+# Space complexity: O(1)
     
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i in range(len(numbers)):
-            diff = target - numbers[i]
-            if diff in hashmap:
-                return [hashmap[diff] + 1, i + 1]
-            hashmap[numbers[i]] = i
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            sum =  numbers[l] + numbers[r]
+            if sum > target:
+                r -= 1
+            elif sum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
