7079b4cf52ca455a5dfa12769a7eae50118342d4
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-07 11:41:41 -0700

    Time: 245 ms (39.96%), Space: 52.4 MB (72.55%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -1,53 +1,33 @@
+// Approach: Sorting + Two Pointers
+// Time Complexity: O(n2)
+// Space Complexity: O(logn) to O(n)
+
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-  if (nums.length < 3) {
-    return [];
-  }
-  // Sort the input array nums
-  const arr = nums.sort((a,b) => a-b);
-  
-  if (arr[0] > 0 || arr[arr.length - 1] < 0) {
-    return [];
-  }
-    
-  const n = arr.length;
-  const res = [];
-  for (let i = 0; i < n; i ++) {
-    // If the current value is greater than zero, break from the loop
-    if (nums[i] > 0) {
-      return res;
-    }
-    // If the current value is the same as the one before, skip it
-    if (i > 0 && arr[i] === arr[i - 1]) {
-      continue;
-    }
-    // Otherwise two sum using two pointers for the current position i
-    let l = i + 1;
-    let r = n - 1;
-    while(l < r) {
-      const temp = arr[i] + arr[l] + arr[r];
-      if (temp > 0) {
-        r --;
-      }
-      if (temp < 0) {
-        l ++;
-      }
-      if (temp === 0) {
-        res.push([nums[i], nums[l], nums[r]]);
-        // skip dup
-        while(l < r && nums[l] === nums[l + 1]) {
-          l ++;
+    let ans = [];
+    const len = nums.length;
+    if(nums == null || len < 3) return ans;
+    nums.sort((a, b) => a - b); 
+    for (let i = 0; i < len ; i++) {
+        if(nums[i] > 0) break; 
+        if(i > 0 && nums[i] == nums[i-1]) continue; 
+        let L = i+1;
+        let R = len-1;
+        while(L < R){
+            const sum = nums[i] + nums[L] + nums[R];
+            if(sum == 0){
+                ans.push([nums[i],nums[L],nums[R]]);
+                while (L<R && nums[L] == nums[L+1]) L++; 
+                while (L<R && nums[R] == nums[R-1]) R--; 
+                L++;
+                R--;
+            }
+            else if (sum < 0) L++;
+            else if (sum > 0) R--;
         }
-        while(l < r && nums[r] === nums[r - 1]) {
-          r --;
-        }
-        l ++;
-        r --;
-      }
-    }
-  }
-  return res;
+    }        
+    return ans;
 };
