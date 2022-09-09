e4e0aa9c29f5c3b3537b3106144cbb032512c912
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-09 08:12:19 -0700

    Time: 182 ms (87.30%), Space: 15.6 MB (34.42%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,11 +1,10 @@
-# Approach 1: HashMap + re module
+# Approach 2: HashMap 
 
 import collections
-import re
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         counts = collections.Counter(nums)
-        return counts.most_common(1)[0][0]
+        return max(counts, key=counts.get) # the key whose value is the largest
         
         
