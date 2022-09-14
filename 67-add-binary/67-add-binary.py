1157e5380337861ded9ce3d5252e1056c88993ad
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-14 15:57:02 -0700

    Time: 61 ms (34.83%), Space: 14 MB (25.80%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -5,6 +5,8 @@
 # Out: '3 is the second argument. yellow is the first'
 # So "{0:b}" is convert the first argument to binary format.
 
+# Time complexity: O((N + M)
+
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         return '{0:b}'.format(int(a,2) + int(b,2))
