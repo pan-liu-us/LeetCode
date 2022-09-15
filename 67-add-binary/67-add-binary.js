4ba718b063f9650ab450b52268b77029bea49dda
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-14 19:56:22 -0700

    Time: 36 ms (91.11%), Space: 14 MB (25.80%) - LeetHub

---
+++ 67-add-binary/67-add-binary.js
@@ -0,0 +1,32 @@
+# Approach 2: Bit-by-Bit Computation
+
+# Time complexity: O(max(N,M)), where N and M are lengths of the input strings a and b.
+# Space complexity:O(max(N,M)) to keep the answer.
+
+class Solution:
+    def addBinary(self, a, b) -> str:
+		    n = max(len(a), len(b))
+		    a, b = a.zfill(n), b.zfill(n)
+		    
+		    carry = 0
+		    answer = []
+		    for i in range(n - 1, -1, -1):
+		        if a[i] == '1':
+		            carry += 1
+		        if b[i] == '1':
+		            carry += 1
+		
+		        if carry % 2 == 1:
+		            answer.append('1')
+		        else:
+		            answer.append('0')
+		
+		        carry //= 2
+		
+		    if carry == 1:
+		        answer.append('1')
+		    answer.reverse()
+		
+		    return ''.join(answer)
+
+        
