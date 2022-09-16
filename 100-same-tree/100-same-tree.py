8a8a36fdcb6a6522aefe1120b333d1d779447c91
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-16 12:34:14 -0700

    Time: 64 ms (12.52%), Space: 14 MB (29.12%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -1,12 +1,12 @@
-# Approach 1: Recursion
+# Approach 2: Iteration
 
-#  Check if p and q nodes are not None, and their values are equal. If all checks are OK, do the same for the child nodes recursively.
+# Start from the root and then at each iteration pop the current node out of the deque. Then do the checks: 1. not None, 2. p.val vs q.val, and if checks are OK, push the child nodes.
 
 # Time complexity: O(N)
 # N is a number of nodes in the tree, since one visits each node exactly once.
 
 # Space complexity: O(N)
-# In the worst case of completely unbalanced tree, to keep a recursion stack.
+# In the worst case of perfect fully balanced binary tree, since BFS will have to store at least an entire level of the tree in the queue, and the last level has O(N) nodes.
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -16,12 +16,31 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
+        
+        def check(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
             return True
-        if not p or not q:
-            return False
-        if p.val != q.val:
-            return False
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+              
+        queue = deque([(p,q)])
+      
+        while queue:
+            p, q = queue.popleft()
+            if not check(p, q):
+                return False
+            if p:
+                queue.append((p.left, q.left))
+                queue.append((p.right, q.right))
+                
+        return True
+            
+        
+        
+
+
             
         
