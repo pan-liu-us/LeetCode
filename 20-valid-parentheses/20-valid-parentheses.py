1435542d1fbf8fa607d82f050e0e19436876b8f9
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-20 12:21:10 -0700

    Time: 84 ms (5.38%), Space: 13.9 MB (26.58%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,20 @@
+# Approach: Stacks
+
+# Time complexity: O(N)
+# traverse the given string one character at a time
+
+# Space complexity: O(N)
+# push all opening brackets onto the stack and in the worst case, all the brackets are opening brackets
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {')':'(',']':'[','}':'{'}
+        stack = []
+        for i in s:
+            if stack and i in dic:
+                if stack[-1] == dic[i]: stack.pop()
+                else: return False
+            else: stack.append(i)
+            
+        return len(stack) == 0
+        
