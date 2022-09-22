3d9086d20293e41a71a47192db0b0949678cb73b
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-21 22:53:13 -0700

    Time: 123 ms (23.02%), Space: 44.1 MB (59.98%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -0,0 +1,37 @@
+// Approach: Iteration
+
+// Time complexity: O(n+m)
+
+// Space complexity: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    let prehead = new ListNode(-1);
+    
+    let prev = prehead;
+    
+    while(list1 && list2) {
+        if (list1.val <= list2.val) {
+            prev.next = list1;
+            list1 = list1.next;
+        } else {
+            prev.next = list2;
+            list2 = list2.next; 
+        }
+        prev = prev.next;
+    }
+    
+    prev.next = list1 || list2;
+    return prehead.next; 
+};
