8e2e9e5bcdba47fbc06cd4cc418827ba526bc367
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-25 01:35:38 -0700

    Time: 34 ms (91.03%), Space: 13.8 MB (96.01%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.js
+++ 226-invert-binary-tree/226-invert-binary-tree.js
@@ -1,35 +1,37 @@
-// Approach 1: Recursive DFS
+# Approach 2: Iterative
 
-// T.C. = O(n)
-// n is the number of nodes in the tree, each node is visited once
+# T.C. = O(n)
+# n is the number of nodes in the tree, each node in the tree is visited / added to the queue only once
 
-// S.C. = O(h) => O(n)
-// h is the height of the tree, because of recursion, O(h) function calls will be placed on the stack in the worst case
+# S.C. = O(n)
+# size of queue, worst case for a full binary tree, the leaf level has n/2 leaves
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {TreeNode}
- */
-var invertTree = function(root) {
-    if (root === null) {
-        return null;
-    }
-    // swap children
-    let temp = root.right;
-    root.right = root.left;
-    root.left = temp;
-
-    // invert child trees
-    invertTree(root.right);
-    invertTree(root.left);
-
-    return root;
-};
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+        
+        # store nodes whose left and right child have not been swapped yet
+        q = collections.deque([root])
+        
+        while q:
+            cur = q.popleft()   
+            cur.left, cur.right = cur.right, cur.left
+            if cur.left:
+                q.append(cur.left)
+            if cur.right:
+                q.append(cur.right)
+        return root
+                
+            
+            
+        
+        
+        
+        
