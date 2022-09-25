f926a8cf285f1b683e7373d9dcaf2a99ac13fe00
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-25 01:39:26 -0700

    Time: 92 ms (50.39%), Space: 42 MB (93.08%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.js
+++ 226-invert-binary-tree/226-invert-binary-tree.js
@@ -1,37 +1,42 @@
-# Approach 2: Iterative
+// Approach 2: Iterative BFS
 
-# T.C. = O(n)
-# n is the number of nodes in the tree, each node in the tree is visited / added to the queue only once
+// T.C. = O(n)
+// n is the number of nodes in the tree, each node in the tree is visited / added to the queue only once
 
-# S.C. = O(n)
-# size of queue, worst case for a full binary tree, the leaf level has n/2 leaves
+// S.C. = O(n)
+// size of queue, worst case for a full binary tree, the leaf level has n/2 leaves
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root:
-            return root
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (!root) {
+        return root;
+    }
+    
+    let q = [];
+    q.push(root);
+    
+    while (q.length) {
+        let cur = q.shift();
+        [cur.left, cur.right] = [cur.right, cur.left]
         
-        # store nodes whose left and right child have not been swapped yet
-        q = collections.deque([root])
-        
-        while q:
-            cur = q.popleft()   
-            cur.left, cur.right = cur.right, cur.left
-            if cur.left:
-                q.append(cur.left)
-            if cur.right:
-                q.append(cur.right)
-        return root
-                
-            
-            
-        
-        
-        
-        
+        if (cur.left) {
+            q.push(cur.left);
+        }
+        if (cur.right) {
+            q.push(cur.right);
+        }
+    }
+    
+    return root;
+};
