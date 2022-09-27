5073127c5c71431bf1b5403f4d981e092d934d24
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-26 23:52:50 -0700

    Time: 515 ms (28.04%), Space: 15.5 MB (26.60%) - LeetHub

---
+++ 704-binary-search/704-binary-search.py
@@ -0,0 +1,26 @@
+# Time Complexity: O(logN)
+# n is the length of nums, while loop will run logN time
+
+# Space Complexity: O(1)
+# the space required by the algorithm to process data is constant
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        if target < nums[l] or target > nums[r]:
+            return -1
+        
+        while l <= r:
+            m = l + (r - l) // 2
+            if nums[m] < target:
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                return m
+        
+        return -1
+        
+        
