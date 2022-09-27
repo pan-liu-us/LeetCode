56a55d67332519d2a5a13260fb7bb64a41af8da1
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-26 22:00:57 -0700

    Time: 82 ms (52.24%), Space: 14.5 MB (67.33%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,23 +1,6 @@
-# Approach 2: Frequency Counter List Size 26
-
-# Time complexity: O(n) 
-# accessing the counter list is a constant time operation
-
-# Space complexity: O(1)
-# the list's size stays constant no matter how large n is.
+# Approach 2: Build-in Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        
-        counter_list = [0] * 26
-        for ch in s:
-            counter_list[ord(ch) - ord('a')] += 1
-        for ch in t:
-            counter_list[ord(ch) - ord('a')] -= 1
-            if counter_list[ord(ch) - ord('a')] < 0:
-                return False
-            
-        return True
+        return Counter(s) == Counter(t)
         
