d5d43b970536a8efee69fa59b1ce494948624209
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-09-26 21:31:50 -0700

    Time: 110 ms (73.89%), Space: 42.1 MB (98.72%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -1,10 +1,10 @@
-// Approach 1: Sorting
+// Approach 2: Counter Array - Size 26
 
-// Time complexity: O(nlogn) 
-// because sorting costs O(nlogn) time and comparing costs O(n) time
+// Time complexity: O(n) 
+// accessing the counter array is a constant time operation
 
 // Space complexity: O(1)
-// it depends on the sorting implementation
+// the array's size stays constant no matter how large n is.
 
 /**
  * @param {string} s
@@ -12,5 +12,20 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return s.length === t.length && [...s].sort().join('') === [...t].sort().join('')
+    if (s.length !== t.length) {
+        return false;
+    }
+    
+    const counter = new Array(26).fill(0);
+    for (let i = 0; i < s.length; i++) {
+        counter[s.codePointAt(i) - 'a'.codePointAt(0)]++;
+    }
+    for (let j = 0; j < t.length; j++) {
+        counter[t.codePointAt(j) - 'a'.codePointAt(0)]--;
+        if (counter[t.codePointAt(j) - 'a'.codePointAt(0)] < 0) {
+            return false;
+        }
+    }
+    
+    return true;
 };
