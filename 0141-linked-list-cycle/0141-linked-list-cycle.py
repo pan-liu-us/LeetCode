036f16570eec07a5270b5e3c140c7bc41358db55
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-10-25 11:40:25 -0700

    Time: 105 ms (57.09%), Space: 17.6 MB (31.19%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -1,11 +1,10 @@
-# Approach 1: Hash Set
+# Approach 2: Two Pointers | Floyd's Cycle Finding Algorithm
+# Time complexity : O(n)
+# List has no cycle: depends on the list's length n, O(n)
+# List has a cycle: depends on both non-cyclic length n and cyclic length m, 0(n+m)
 
-# Time complexity: O(n)
-# We visit each of the n elements in the list at most once. 
-# Adding a node to the hash table costs only O(1) time.
-
-# Space complexity: O(n)
-# At most n elements can be added to the hash set.
+# Space complexity : O(1)
+# We only use two nodes, slow and fast
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -14,13 +13,17 @@
 #         self.next = None
 
 class Solution:
-    def hasCycle(self, head: ListNode) -> bool:
-        nodes_seen = set()
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head == None:
+            return False
+        
+        slow = head
+        fast = head
 
-        while head is not None:
-            if head in nodes_seen:
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
                 return True
-            nodes_seen.add(head)
-            head = head.next
 
         return False
