578557d8e7c37867308a0739d88a5da625b629b7
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-10-29 01:53:01 -0700

    Time: 179 ms (57.05%), Space: 50 MB (36.00%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.js
@@ -0,0 +1,47 @@
+// One Stack Using Nested Array
+
+// T.C. = O(1)
+// S.C. = O(n)
+
+var MinStack = function() {
+    this.stack = []
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push([val, this.stack.length ? Math.min(val, this.getMin()) : val])
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop();   
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1][0];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.stack[this.stack.length - 1][1];
+    
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
