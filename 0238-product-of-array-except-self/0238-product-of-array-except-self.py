313b0f63fdd3907738372e64ca7d748491d01f9c
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-10-31 15:11:38 -0700

    Time: 228 ms (97.81%), Space: 21.2 MB (81.90%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,25 +1,19 @@
-# Left and Right List
-# Time complexity:O(N)
-# N represents the number of elements in the input array. We use three iterations in total.
-# To construct the array L, array R, and the answer array using L and R.
+# O(1) space approach
 
-# Space complexity:O(N)
-# Used up by the two intermediate arrays to keep track of the product of elements to the left and right.
+# Time complexity: O(N)
+# We use one iteration to construct the array L, and one to update the res array.
+
+# Space complexity: O(1) 
+# If the res array does not count as extra space
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = []
-        L = [1] * len(nums)
-        R = [1] * len(nums)
-
+        res = [1] * len(nums)
         for i in range(1, len(nums)):
-            L[i] = L[i - 1] * nums[i - 1]
-
-        for j in range(len(nums) - 2, -1, -1):
-            R[j] = R[j + 1] * nums[j + 1]
-
-        for num1, num2 in zip(L, R):
-            res.append(num1 * num2)
-        
+            res[i] = res[i - 1] * nums[i - 1]
+        R = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] = res[i] * R
+            R *= nums[i]
         return res
         
