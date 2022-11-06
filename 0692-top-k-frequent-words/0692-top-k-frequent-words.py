9ae555ad0ccc5d9af7757930c4d43ea1b2220acf
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-11-05 19:26:54 -0700

    Time: 65 ms (85.99%), Space: 14 MB (25.12%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,7 +1,8 @@
 # Brute Force
-
-# Time complexity: O(NlogN)use sorting
-# Space complexity: O(N) store frequencies in a HashMap and return a slice from a sorted list
+# Time complexity: O(N + NlogN) 
+# count the frequency of each word in O(N),use sorting O(NlogN)
+# Space complexity: O(N) 
+# store frequencies in a HashMap and return a slice from a sorted list
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
@@ -12,13 +13,4 @@ class Solution:
         def sort_key(x):
             return (-cnt[x], x) # first sort by frequcy, then sort by the word itself
 
-        return sorted(list(cnt.keys()), key=sort_key)[:k]
-        
-
-
-
-
-
-
-        
-        
+        return sorted(list(cnt.keys()), key=sort_key)[:k] # key=lambda x: (-cnt[x], x)
