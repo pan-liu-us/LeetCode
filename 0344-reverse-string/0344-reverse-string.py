2ece64c5dad3c974b010f6399f42ae443198ac40
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-11-14 15:12:10 -0800

    Time: 211 ms (88.66%), Space: 18.5 MB (19.19%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,4 +1,14 @@
+# Two Pointers
+
+# Time complexity: O(N) to swap N/2 element.
+# Space complexity: O(1)
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        s.reverse()
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left = left + 1
+            right = right - 1
         
