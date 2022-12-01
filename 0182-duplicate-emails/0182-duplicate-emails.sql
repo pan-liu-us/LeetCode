c58e7024201a820eb621a0c241d3bb20165a1a3d
Pan Liu <64609529+pan-liu-us@users.noreply.github.com>
2022-12-01 12:20:03 -0800

    Time: 497 ms (39.22%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
 SELECT email 
-FROM (SELECT email, COUNT(email) AS cnt FROM Person GROUP BY email) t
-WHERE t.cnt > 1
+FROM Person 
+GROUP BY email 
+HAVING COUNT(email) > 1;
